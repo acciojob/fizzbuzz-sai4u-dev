da52990902ecc35e51428d4238c3b9a2b48ecbcb
Sai Narendra <tannirusainarendra81@gmail.com>
2025-08-09 18:19:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+function fizzBuzz() {
+  let result = "";
+
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      result += "FizzBuzz";
+    } else if (i % 3 === 0) {
+      result += "Fizz";
+    } else if (i % 5 === 0) {
+      result += "Buzz";
+    } else {
+      result += i;
+    }
+
+    if (i !== 100) {
+      result += "\n";
+    }
+  }
+	
+  alert(result);
+}
+
+fizzBuzz();
